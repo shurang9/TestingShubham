ec05f8bf4a6ddca113f500853ee86c3b958f1990
Shubham <66883450+shurang9@users.noreply.github.com>
2025-03-05 13:05:19 +0530

    First push
    First push

---
+++ TestingShubham/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingShubham
+//
+//  Created by apple on 05/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestingShubhamTests/TestingShubhamTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestingShubhamTests.swift
+//  TestingShubhamTests
+//
+//  Created by apple on 05/03/2025.
+//
+
+import Testing
+@testable import TestingShubham
+
+struct TestingShubhamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
