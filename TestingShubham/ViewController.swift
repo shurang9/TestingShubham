dc3322412bc9290c30237e9e8849cfc387c3bb6f
Shubham <66883450+shurang9@users.noreply.github.com>
2025-03-05 13:09:38 +0530

    Print(BB)
     Print(BB)

--- TestingShubham/ViewController.swift
+++ TestingShubham/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Print(BB)
         // Do any additional setup after loading the view.
     }
 
